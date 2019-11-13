07562b8857b962365e858462af9c5e6b972dfde2
Mahmoud Montaser <y7odag@gmail.com>
2019-11-13 12:49:25 -0500

    Updating Models

---
+++ JMS.DAL/Models/Checkpoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JMS.DAL.Models
+{
+    public partial class Checkpoint
+    {
+        public Checkpoint()
+        {
+            JourneyUpdate = new HashSet<JourneyUpdate>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Lat { get; set; }
+        public string Lng { get; set; }
+
+        public virtual ICollection<JourneyUpdate> JourneyUpdate { get; set; }
+    }
+}
